0868cbe24823948cec0bce5d35ddae2c65f37ac2
mkrtichminasyan <mkrtichminasyan1992@gmail.com>
2022-08-16 14:42:02 +0400

    first commit

--- src/Components/TodoCard.js
+++ src/Components/TodoCard.js
@@ -7,6 +7,7 @@ import {
     Card,
     CardContent,
     Chip,
+    Grid,
     Stack,
     Typography
 } from '@mui/material'
@@ -15,34 +16,43 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 
-const TodoCard = ({ title, description, priority, handleEdit,handleDelete }) => {
+const TodoCard = ({ title, description, priority, handleEdit, handleDelete }) => {
 
     return (
         <Card sx={{ mb: 3 }}>
             <CardContent>
-                <Stack direction='row' spacing={2}>
-                    <Chip label={title} color="primary" size="small" />
-                    <Chip label={priority} color="secondary" size="small" />
-                    <Button
-                        variant="contained"
-                        startIcon={<EditIcon />}
-                        size='small'
-                        sx={{ borderRadius: 8 }}
-                        onClick={handleEdit}
-                    >
-                        Edit
-                    </Button>
-                    <Button
-                        color='error'
-                        variant="contained"
-                        startIcon={<DeleteIcon />}
-                        size='small'
-                        sx={{ borderRadius: 8 }}
-                        onClick={handleDelete}
-                    >
-                        Delete
-                    </Button>
-                </Stack>
+                <Grid container display={"flex"} justifyContent={"center"}>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <Chip label={title} color="primary" size="small" />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <Chip label={priority} color="secondary" size="small" />
+                    </Grid>
+
+                    <Grid item xs={12} sm={6} md={3}>
+                        <Button
+                            variant="contained"
+                            startIcon={<EditIcon />}
+                            size='small'
+                            sx={{ borderRadius: 8 }}
+                            onClick={handleEdit}
+                        >
+                            Edit
+                        </Button>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <Button
+                            color='error'
+                            variant="contained"
+                            startIcon={<DeleteIcon />}
+                            size='small'
+                            sx={{ borderRadius: 8 }}
+                            onClick={handleDelete}
+                        >
+                            Delete
+                        </Button>
+                    </Grid>
+                </Grid>
                 <Accordion sx={{ mt: 2 }}>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
